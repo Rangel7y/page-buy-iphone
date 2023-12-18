ec235037903c999b33d2dd3f2815a1fe6408cbbc
f4t4l777 <rangel0011@outlook.com>
2023-12-18 17:19:18 -0300

    feat: added function to toggle img product tablist

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+/* Get Buttons in Tablist Picker */
+const btnTablist = document.querySelectorAll("#tablist ion-icon.btn-picker");
+
+/* Get Image Product */
+const imgProduct = document.getElementById('img-product');
+
+btnTablist.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        btnTablist.forEach((btn) => btn.classList.remove("selected"));
+        btn.classList.add("selected");
+
+        switch(e.target.getAttribute("id")){
+            case 'index-0':
+                imgProduct.src = "./assets/all_models1.png";
+                break;
+            case 'index-1':
+                imgProduct.src = "./assets/model_black/2_894424-2352x2911.webp";
+                break;
+            case 'index-2':
+                imgProduct.src = "./assets/model_white/2_894433-2352x2911.webp";
+                break; 
+            case 'index-3':
+                imgProduct.src = "./assets/model_blue/2_894448-2350x2909.webp";
+                break;   
+            case 'index-4':
+                imgProduct.src = "./assets/model_gray/2_894439-2350x2908.webp";
+                break;
+            default:
+                break;
+        }
+        
+    });
+});
